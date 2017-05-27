72ceea1a5c17c3f81f9f8aa896debbec6f02ec3b
Michael Dolas <lolshockah@gmail.com>
2017-05-27 12:44:34 +0200

    XInput controller provider WIP

---
+++ xinput/src/pl/shockah/godwit/controllers/xinput/XInputControllerProvider.groovy
@@ -0,0 +1,23 @@
+package pl.shockah.godwit.controllers.xinput
+
+import groovy.transform.CompileStatic
+import pl.shockah.godwit.controllers.Controller
+import pl.shockah.godwit.controllers.ControllerProvider
+
+@CompileStatic
+class XInputControllerProvider implements ControllerProvider {
+    @Override
+    List<Controller> getControllers() {
+        return null
+    }
+
+    @Override
+    void onUpdate() {
+
+    }
+
+    @Override
+    void postUpdate() {
+
+    }
+}
